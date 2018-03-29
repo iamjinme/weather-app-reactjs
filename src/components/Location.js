7187d2c1043ec46003570eae9867dded0cdd9f4a
Jinme Mirabal <mirabalj@gmail.com>
2018-03-29 11:13:24 -0500

    Section 3. Chapter 24.

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Location = ({ city = 'None' }) => <div><h1>{city}</h1></div>;
+
+Location.propTypes = {
+  city: PropTypes.string.isRequired,
+}
 
 export default Location;
